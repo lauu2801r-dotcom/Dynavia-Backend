d96b3179e9db98f35fd4349cffb849901b3739c5
lauu2801r-dotcom <lauu2801r@gmail.com>
2026-05-17 13:00:35 +0000

    feat: MQTT Mosquitto interno funcional, flujo semaforos white/normal verificado

--- ms-traffic/index.js
+++ ms-traffic/index.js
@@ -20,7 +20,7 @@ pool.connect((err) => {
 });
 
 // Conexión MQTT Mosquitto
-const mqttClient = mqtt.connect(process.env.MQTT_BROKER || 'mqtt://broker.emqx.io:1883');
+const mqttClient = mqtt.connect(process.env.MQTT_BROKER || 'mqtt://mosquitto:1883');
 
 mqttClient.on('connect', () => {
   console.log('✅ ms-traffic conectado a Mosquitto MQTT');
